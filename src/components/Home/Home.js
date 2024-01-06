67ceb601090e6f6fb1385af9c9eb4150b7cdfaee
Keerthy Baby <keerthybaby025@gmail.com>
2024-01-06 23:09:23 +0530

    useDispatch to dispatch action

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,14 @@ import React, { useEffect } from 'react';
 import MovieListing from "../MovieListing/MovieListing"
 import movieApi from "../../common/apis/MovieApi";
 import {APIKey}  from '../../common/apis/MovieApiKey';
-
+import {useDispatch} from "react-redux";
+import {addMovies} from "../../features/movies/movieSlice"
 
 
 const Home = () => {
 
+  const movieText="Harry";
+  const dispatch=useDispatch();
   useEffect(()=>{
     const movieText="Harry";
     const fetchMovies= async () =>{
@@ -15,7 +18,7 @@ const Home = () => {
       .catch((err)=>{
         console.log("Err :",err);
       });
-      console.log("The response from API",response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
   },[]);
