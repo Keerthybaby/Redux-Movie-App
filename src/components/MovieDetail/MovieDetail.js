3e7a61e0367628ce50b2a30ef526e096381fa038
Keerthy Baby <keerthybaby025@gmail.com>
2024-01-10 01:58:49 +0530

    Build Movie Carousel

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -20,11 +20,15 @@ const MovieDetail = () => {
     };
   }, [dispatch, imdbID]);
 
-  if (!data) {
-    return <div>Loading...</div>;
-  }
+  // if (!data) {
+  //   return <div>Loading...</div>;
+  // }
   return (
     <div className="movie-section">
+      {Object.keys(data).length===0 ? (
+        <div>...Loading</div>
+      ):(
+      <>
       <div className="section-left">
         <div className="movie-title">{data.Title}</div>
         <div className="movie-rating">
@@ -68,6 +72,8 @@ const MovieDetail = () => {
       <div className="section-right">
         <img src={data.Poster} alt={data.Title} />
       </div>
+      </>
+      )}
     </div>
   );
 };
